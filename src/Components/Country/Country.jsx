d69b1650b12e748a9d90b4895873d1795b33d882
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2025-03-05 16:02:34 +0600

    toggle button using usestate

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import "./Country.css"
 const Country = ({country}) => {
     const {name,flags}=country
-    console.log(country)
+    // console.log(country)
+    const [Visit,setVisit]=useState(false)
+    function handleVisited(){
+        setVisit(!Visit)
+    }
     return (
         <div className="country">
             <h3>Name:{name.common}</h3>
             <img src={flags.png} alt="" />
+            <button onClick={handleVisited} className="btn">{Visit?'visited':'visit'}</button>
+            
         </div>
+
+            
     );
 };
 
